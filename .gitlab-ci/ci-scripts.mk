c586a25213258fac0bde536bd36b661ad0836e1b
utzcoz <utzcoz@outlook.com>
2023-10-28 19:51:44 +0800

    ci: Fix indent of ci-scripts.mk
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- .gitlab-ci/ci-scripts.mk
+++ .gitlab-ci/ci-scripts.mk
@@ -8,7 +8,7 @@
 FILES_IN_SUBDIR := \
     .gitlab-ci/distributions \
     .gitlab-ci/reprepro.sh \
-	.gitlab-ci/install-android-sdk.sh \
+    .gitlab-ci/install-android-sdk.sh \
 
 CONFIG_FILE := .gitlab-ci/config.yml
 OUTPUTS := .gitlab-ci.yml \
