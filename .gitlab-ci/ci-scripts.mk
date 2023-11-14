ac6e9ad480792074eab32db0d4f41d64b1bc932b
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-11-14 12:06:57 -0600

    ci: Move and rename templates for clearer separation
    Avoids having templates in the same place as the files they generate.
    
    Also rename the files that just get included

--- .gitlab-ci/ci-scripts.mk
+++ .gitlab-ci/ci-scripts.mk
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2022 Collabora, Ltd. and the Monado contributors
+# SPDX-FileCopyrightText: 2022-2023 Collabora, Ltd. and the Monado contributors
 #
 # To generate all the templated files, run this from the root of the repo:
 #   make -f .gitlab-ci/ci-scripts.mk
 
-# These also all have their template named the same with a .jinja suffix.
+# These also all have their template named the same with a .jinja suffix in the template subdir.
 FILES_IN_SUBDIR := \
     .gitlab-ci/distributions \
     .gitlab-ci/reprepro.sh \
@@ -24,14 +24,15 @@ clean:
 
 CI_FAIRY := ci-fairy generate-template --config=$(CONFIG_FILE)
 
-# As the default thing for ci-fairy to template, this is special cased
-.gitlab-ci.yml: .gitlab-ci/ci.template .gitlab-ci/win_containers.yml $(CONFIG_FILE)
+# Because we have includes, and it goes to a different directory, this is special cased
+.gitlab-ci.yml: .gitlab-ci/templates/.gitlab-ci.yml.jinja $(CONFIG_FILE)
 	$(CI_FAIRY) $< > $@
 # Extra deps
-.gitlab-ci.yml: .gitlab-ci/ci-containers-variables.yml.jinja
-.gitlab-ci.yml: .gitlab-ci/ci-containers.yml.jinja
-.gitlab-ci.yml: .gitlab-ci/functions.jinja
+.gitlab-ci.yml: .gitlab-ci/templates/include.win_containers.yml
+.gitlab-ci.yml: .gitlab-ci/templates/include.ci-containers-variables.yml.jinja
+.gitlab-ci.yml: .gitlab-ci/templates/include.ci-containers.yml.jinja
+.gitlab-ci.yml: .gitlab-ci/templates/include.functions.jinja
 
 # Everything else is structured alike
-$(FILES_IN_SUBDIR): %: %.jinja $(CONFIG_FILE)
+$(FILES_IN_SUBDIR): .gitlab-ci/%: .gitlab-ci/templates/%.jinja $(CONFIG_FILE)
 	$(CI_FAIRY) $< > $@
