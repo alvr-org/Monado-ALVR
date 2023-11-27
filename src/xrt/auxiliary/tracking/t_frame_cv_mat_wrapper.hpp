e9d8e0db219d5b54d400f21f8b4d822ced02cb94
Jakob Bornecrantz <jakob@collabora.com>
2023-11-27 19:46:34 +0000

    a/tracking: Tidy and document FrameMat [NFC]

--- src/xrt/auxiliary/tracking/t_frame_cv_mat_wrapper.hpp
+++ src/xrt/auxiliary/tracking/t_frame_cv_mat_wrapper.hpp
@@ -1,4 +1,4 @@
-// Copyright 2021, Collabora, Ltd.
+// Copyright 2021-2023, Collabora, Ltd.
 // SPDX-License-Identifier: BSL-1.0
 /*!
  * @file
@@ -15,12 +15,15 @@
 namespace xrt::auxiliary::tracking {
 
 
+/*!
+ * This class implements the @ref xrt_frame interface, allowing interfacing to
+ * @p cv::Mat from C code. Keeps a reference to the cv::Mat and so the data
+ * alive, this class is freed when the reference count reaches zero.
+ */
 class FrameMat
 {
 public:
-	/*!
-	 * Additional optional parameters for frame creation.
-	 */
+	//! Additional optional parameters for frame creation.
 	class Params
 	{
 	public:
@@ -28,16 +31,14 @@ public:
 		uint64_t timestamp_ns;
 	};
 
-
-
-	// Exposed to the C api.
+	//! Exposed @ref xrt_frame API.
 	struct xrt_frame frame = {};
 
 	// The cv::Mat that holds the data.
 	cv::Mat matrix = cv::Mat();
 
 
-
+public: // Methods
 	/*!
 	 * Only public due to C needed to destroy it.
 	 */
