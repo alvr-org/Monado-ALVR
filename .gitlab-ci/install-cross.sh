66a5b36bd5ec0f50e09a4fba314c523a6e6bdf46
Simon Zeni <simon.zeni@collabora.com>
2024-07-04 13:17:13 -0400

    ci: add more dependencies to install-cross.sh, and bump CI image
    Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2272>

--- .gitlab-ci/install-cross.sh
+++ .gitlab-ci/install-cross.sh
@@ -22,20 +22,27 @@ for arch in $CROSS_ARCHITECTURES; do
     # they apparently can't be installed in both architectures at once
     apt-get install -y --no-install-recommends --no-remove \
             "crossbuild-essential-${arch}" \
-            "libelf-dev:${arch}" \
+            "libavcodec-dev:${arch}" \
+            "libbsd-dev:${arch}" \
+            "libcjson-dev:${arch}" \
             "libegl1-mesa-dev:${arch}" \
+            "libeigen3-dev:${arch}" \
+            "libelf-dev:${arch}" \
             "libgl1-mesa-dev:${arch}" \
             "libglvnd-dev:${arch}" \
             "libhidapi-dev:${arch}" \
+            "libsdl2-dev:${arch}" \
             "libudev-dev:${arch}" \
             "libusb-1.0-0-dev:${arch}" \
             "libv4l-dev:${arch}" \
             "libvulkan-dev:${arch}" \
             "libwayland-dev:${arch}" \
             "libx11-dev:${arch}" \
+            "libx11-xcb-dev:${arch}" \
             "libxcb-randr0-dev:${arch}" \
             "libxrandr-dev:${arch}" \
             "libxxf86vm-dev:${arch}"
+
 
     if [ "$arch" != "i386" ]; then
         mkdir "/var/cache/apt/archives/${arch}"
